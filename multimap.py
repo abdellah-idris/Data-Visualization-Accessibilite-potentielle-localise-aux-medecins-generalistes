c8f1f6e128317435941b76c897eccdbc3c98f3b7
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2023-11-12 21:21:02 +0100

    add 2019 & 2021 data

--- multimap.py
+++ multimap.py
@@ -1,18 +1,18 @@
-from mock import departement_apl_2015, departement_apl_2016, departement_apl_2017, departement_apl_2018
+from mock import departement_apl_2015, departement_apl_2016, departement_apl_2017, departement_apl_2018, departement_apl_2019, departement_apl_2021
 from flask import Flask, render_template, request, jsonify
 import branca.colormap as cm
-import json
 
 app = Flask(__name__)
 
 # Define a color scale for the choropleth using a LinearColormap
 color_scale = cm.LinearColormap(['yellow', 'red'], vmin=0, vmax=1)
 
+
 @app.route('/')
 def index():
     # Load the default map for a selected year
-    year = "2018"  # Default year
-    apl = departement_apl_2018  # Default data
+    year = "2015"  # Default year
+    apl = departement_apl_2015  # Default data
 
     # Prepare the data as a GeoJSON FeatureCollection
     apl_dict = {
@@ -37,8 +37,10 @@ def index():
 
     return render_template('map.html', selected_year=year, apl_data=jsonify(apl_dict))
 
+
 @app.route('/get_map', methods=['POST'])
 def get_map():
+    apl = None
     if request.method == 'POST':
         year = request.form.get('year')
     else:
@@ -53,6 +55,10 @@ def get_map():
         apl = departement_apl_2017
     elif year == '2018':
         apl = departement_apl_2018
+    elif year == '2019':
+        apl = departement_apl_2019
+    elif year == '2021':
+        apl = departement_apl_2021
 
     apl_dict = {
         'type': 'FeatureCollection',
@@ -66,8 +72,8 @@ def get_map():
                 'code': row['code'],
                 'nom_departement': row['nom_departement'],
                 'Weighted APL': row['Weighted APL'],
-                'Population standardisée '+ str(int(year)-2) + ' pour la médecine générale': row[
-                    'Population standardisée '+ str(int(year)-2) + ' pour la médecine générale'],
+                'Population standardisée ' + str(int(year)-2) + ' pour la médecine générale': row[
+                    'Population standardisée ' + str(int(year)-2) + ' pour la médecine générale'],
                 'Weighted Average APL': row['Weighted Average APL'],
             },
             'geometry': row['geometry'].__geo_interface__
